a08f14f71802061514f60325bfc8d29fd8ca36ba
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-07-17 18:59:26 -0500

    149. Conectando nuestra base de datos con nuestra aplicación

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,23 @@
-import express from 'express' 
-import colors from 'colors'
-import morgan from 'morgan'
+import express, { Express } from 'express'; 
+import colors from 'colors';
+import morgan from 'morgan';
+import { db } from './config/db';
 
-const app = express()
+async function connectDB () {
+    try {
+        await db.authenticate();
+        db.sync();
+        console.log(colors.blue.bold('Base de datos conectada'));
+    } catch (error) {
+        console.log(colors.red.bold('No se pudo conectar a la base de datos'));
+        process.exit(1); // Detiene el proceso
+    }
+};
+connectDB();
+const app: Express = express();
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
-app.use(express.json())
+app.use(express.json());
 
-
-
-export default app
+export default app;
