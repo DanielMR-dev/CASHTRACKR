f8e7341c36b08f520c411c7791b3efae19dcc398
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-07-21 15:45:01 -0500

    156. Agregando validación a la creación de Presupuestos

--- backend/src/routes/budgetRouter.ts
+++ backend/src/routes/budgetRouter.ts
@@ -1,13 +1,29 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { BudgetController } from "../controllers/BudgetController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router: Router = Router();
 
 // Routes
 router.get('/', BudgetController.getAll);
-router.post('/', BudgetController.create);
+
+router.post('/', 
+    body('name')
+        .notEmpty().withMessage('El nombre del presupuesto es requerido')
+        .isString().withMessage('El nombre debe ser una cadena de texto'),
+    body('amount')
+        .notEmpty().withMessage('La cantidad del presupuesto es requerido')
+        .isNumeric().withMessage('La cantidad debe ser un número')
+        .custom((value) => value > 0).withMessage('La cantidad debe ser mayor a 0'), // Se debe evaluar como "false", no como "true"
+    handleInputErrors,
+    BudgetController.create
+);
+
 router.get('/:id', BudgetController.getBudgetById);
+
 router.put('/:id', BudgetController.updateBudgetById);
+
 router.delete('/:id', BudgetController.deleteBudgetById);
 
 export default router;
