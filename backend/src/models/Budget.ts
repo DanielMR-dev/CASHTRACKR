9d53ab2fbb8e5de762929f18e06103e55bca6158
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-07-23 17:10:00 -0500

    167. Creando el Modelo de Gastos

--- backend/src/models/Budget.ts
+++ backend/src/models/Budget.ts
@@ -1,15 +1,22 @@
 import { Table, Column, Model, DataType, HasMany, BelongsTo, ForeignKey } from 'sequelize-typescript';
+import Expense from './Expense';
 
 @Table({
     tableName: 'budgets'
 })
 
-export class Budget extends Model {
+class Budget extends Model {
     @Column({ type: DataType.STRING(100) })
     declare name: string;
 
     @Column({ type: DataType.DECIMAL })
     declare amount: number;
+
+    @HasMany(() => Expense, {
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+    })
+    declare expenses: Expense[];
 };
 
 export default Budget;
