38a88612aa6c5a7350e93a0d4ac47e594d860754
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-07-23 21:11:53 -0500

    180. Hashear los passwords de los usuarios

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,9 +1,10 @@
 import type { Request, Response } from "express";
 import User from "../models/User";
+import { hashPassword } from "../utils/auth";
 
 export class AuthController { 
     static createAccount = async (req: Request, res: Response) => {
-        const { email} = req.body;
+        const { email, password } = req.body;
         // Prevenir duplicados
         const userExists = await User.findOne({ where: { email } });
         if (userExists) {
@@ -11,6 +12,7 @@ export class AuthController {
         }
         try {
             const user = new User(req.body);
+            user.password = await hashPassword(password);
             await user.save();
             res.status(201).json({ message: '¡Tu cuenta ha sido creada correctamente!' });
         } catch (error) {
