1bc6bc43358edd326cc9f9091dcec95d96e48eac
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-07-23 20:59:40 -0500

    178. Registrando Usuarios

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,7 +1,15 @@
 import type { Request, Response } from "express";
+import User from "../models/User";
 
 export class AuthController { 
     static createAccount = async (req: Request, res: Response) => {
-        res.json('Creando Cuenta...');
+        try {
+            const user = new User(req.body);
+            await user.save();
+            res.status(201).json({ message: 'Cuenta creada correctamente' });
+        } catch (error) {
+            // console.log(error);
+            res.status(500).json({ error: 'Error al crear la cuenta' });
+        }
     }
 }
