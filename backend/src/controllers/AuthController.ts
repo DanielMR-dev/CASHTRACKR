86f58705b99880a709cd887c530daf0484ce8733
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-07-23 21:04:53 -0500

    179. Revisar que un usuario exista antes de registrarlo

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -3,10 +3,16 @@ import User from "../models/User";
 
 export class AuthController { 
     static createAccount = async (req: Request, res: Response) => {
+        const { email} = req.body;
+        // Prevenir duplicados
+        const userExists = await User.findOne({ where: { email } });
+        if (userExists) {
+            return res.status(409).json({ error: 'Un usuario con ese email ya existe' });
+        }
         try {
             const user = new User(req.body);
             await user.save();
-            res.status(201).json({ message: 'Cuenta creada correctamente' });
+            res.status(201).json({ message: '¡Tu cuenta ha sido creada correctamente!' });
         } catch (error) {
             // console.log(error);
             res.status(500).json({ error: 'Error al crear la cuenta' });
