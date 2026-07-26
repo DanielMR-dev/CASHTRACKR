80a255855d50829ed2fa55fced2dacfb291563df
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-07-25 19:34:05 -0500

    219. Añadiendo Testing para los otros usuarios

--- backend/src/tests/unit/BudgetController.test.ts
+++ backend/src/tests/unit/BudgetController.test.ts
@@ -24,4 +24,38 @@ describe('BudgetController.getAll', () => {
         expect(res.statusCode).toBe(200);
         expect(res.statusCode).not.toBe(404);
     });
+
+    test('Should retrieve 1 budget for user with ID 2', async () => {
+        const req = createRequest({
+            method: 'GET',
+            url: '/api/budgets',
+            user: { id: 2 }
+        });
+        const res = createResponse();
+
+        const filteredBudgets = budgets.filter(budget => budget.userId === req.user?.id);
+        (Budget.findAll as jest.Mock).mockResolvedValue(filteredBudgets);
+        await BudgetController.getAllBudgets(req, res);
+        const data = res._getJSONData();
+        expect(data).toHaveLength(1);
+        expect(res.statusCode).toBe(200);
+        expect(res.statusCode).not.toBe(404);
+    });
+
+    test('Should retrieve 0 budgets for user with ID 10', async () => {
+        const req = createRequest({
+            method: 'GET',
+            url: '/api/budgets',
+            user: { id: 10 }
+        });
+        const res = createResponse();
+
+        const filteredBudgets = budgets.filter(budget => budget.userId === req.user?.id);
+        (Budget.findAll as jest.Mock).mockResolvedValue(filteredBudgets);
+        await BudgetController.getAllBudgets(req, res);
+        const data = res._getJSONData();
+        expect(data).toHaveLength(0);
+        expect(res.statusCode).toBe(200);
+        expect(res.statusCode).not.toBe(404);
+    });
 });
