5babcde2355c73a999be4a30e43b3db67240ac5b
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-07-26 18:45:41 -0500

    224. Testing a Obtener por ID

--- backend/src/tests/unit/BudgetController.test.ts
+++ backend/src/tests/unit/BudgetController.test.ts
@@ -2,16 +2,17 @@ import { createRequest, createResponse } from "node-mocks-http";
 import { budgets } from "../mocks/budgets";
 import { BudgetController } from "../../controllers/BudgetController";
 import Budget from "../../models/Budget";
+import Expense from "../../models/Expense";
 
 jest.mock('../../models/Budget', () => ({ 
     findAll: jest.fn(),
-    create: jest.fn()
+    create: jest.fn(),
+    findByPk: jest.fn()
 }));
 
 describe('BudgetController.getAll', () => {
 
     beforeEach(() => {
-        // Reset the Mock
         (Budget.findAll as jest.Mock).mockReset();
         // Mock the findAll method to filter budgets by userId
         (Budget.findAll as jest.Mock).mockImplementation((options) => {
@@ -74,7 +75,7 @@ describe('BudgetController.getAll', () => {
         const res = createResponse();
         (Budget.findAll as jest.Mock).mockRejectedValueOnce(new Error());
         await BudgetController.getAllBudgets(req, res);
-        
+
         expect(res.statusCode).toBe(500);
         expect(res._getJSONData()).toStrictEqual({ error: 'Error al obtener los presupuestos' });
     });
@@ -128,5 +129,67 @@ describe('BudgetController.createBudget', () => {
         expect(data).toStrictEqual({ error: 'Error al Crear el presupuesto' });
         expect(mockBudget.save).not.toHaveBeenCalled();
         expect(Budget.create).toHaveBeenCalledWith(req.body);
+    });
+});
+
+describe('BudgetController.getBudgetById', () => {
+
+    beforeEach(() => {
+        (Budget.findByPk as jest.Mock).mockReset();
+        // Mock the findByPk method to return a budget with the especified ID
+        (Budget.findByPk as jest.Mock).mockImplementation((id) => {
+            const budget = budgets.filter(budget => budget.id === id)[0];
+            return Promise.resolve(budget);
+        });
+    });
+
+    test('Should return a budget with ID 1 and 3 expenses', async () => {
+        const req = createRequest({
+            method: 'GET',
+            url: '/api/budgets/:id',
+            budget: { id: 1 }
+        });
+        const res = createResponse();
+        await BudgetController.getBudgetById(req, res);
+        const data = res._getJSONData();
+
+        expect(res.statusCode).toBe(200);
+        expect(res.statusCode).not.toBe(404);
+        expect(data.expenses).toHaveLength(3);
+        expect(Budget.findByPk).toHaveBeenCalled();
+        expect(Budget.findByPk).toHaveBeenCalledTimes(1);
+        expect(Budget.findByPk).toHaveBeenCalledWith(req.budget!.id, {
+            include: [Expense]
+        });
+    });
+
+    test('Should return a budget with ID 2 and 2 expenses', async () => {
+        const req = createRequest({
+            method: 'GET',
+            url: '/api/budgets/:id',
+            budget: { id: 2 }
+        });
+        const res = createResponse();
+        await BudgetController.getBudgetById(req, res);
+        const data = res._getJSONData();
+
+        expect(res.statusCode).toBe(200);
+        expect(res.statusCode).not.toBe(404);
+        expect(data.expenses).toHaveLength(2);
+    });
+
+    test('Should return a budget with ID 3 and 0 expenses', async () => {
+        const req = createRequest({
+            method: 'GET',
+            url: '/api/budgets/:id',
+            budget: { id: 3 }
+        });
+        const res = createResponse();
+        await BudgetController.getBudgetById(req, res);
+        const data = res._getJSONData();
+
+        expect(res.statusCode).toBe(200);
+        expect(res.statusCode).not.toBe(404);
+        expect(data.expenses).toHaveLength(0);
     });
 });
