7fbd116f98a068b3de493c7e37669120d9900d37
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-07-26 19:48:48 -0500

    231. Testing cuado un usuario intenta acceder a un presupuesto que no tiene acceso

--- backend/src/tests/unit/middleware/budget.test.ts
+++ backend/src/tests/unit/middleware/budget.test.ts
@@ -75,4 +75,20 @@ describe('Budget Middleware - hasAccess', () => {
         expect(next).toHaveBeenCalled();
         expect(next).toHaveBeenCalledTimes(1);
     });
+
+    test('Should return 401 error if userId does not have access to budget', () => {
+        const req = createRequest({
+            budget: budgets[0],
+            user: { id: 2 }
+        });
+        const res = createResponse();
+        const next = jest.fn();
+
+        hasAccess(req, res, next);
+
+        const data = res._getJSONData();
+        expect(res.statusCode).toBe(401);
+        expect(data).toStrictEqual({ error: 'Acción no autorizada' });
+        expect(next).not.toHaveBeenCalled();
+    });
 });
