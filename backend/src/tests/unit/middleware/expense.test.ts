d7aa39907c46606bb207c13168dd167766f3cf47
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-07-27 19:44:28 -0500

    238. Testing a un gasto que no existe

--- backend/src/tests/unit/middleware/expense.test.ts
+++ backend/src/tests/unit/middleware/expense.test.ts
@@ -8,5 +8,27 @@ jest.mock('../../../models/Expense', () => ({
 }));
 
 describe('Expenses Middleware - validateExpenseExists', () => {
-    
+    beforeEach(() => {
+        (Expense.findByPk as jest.Mock).mockReset();
+        (Expense.findByPk as jest.Mock).mockImplementation((id) => {
+            const expense = expenses.filter(expense => expense.id === id)[0] ?? null;
+            return Promise.resolve(expense);
+        });
+    });
+
+    test('Should handle non-existent expense', async () => {
+        const req = createRequest({
+            params: {
+                expenseId: 10
+            }
+        });
+        const res = createResponse();
+        const next = jest.fn();
+        await validateExpenseExists(req, res, next);
+        const data = res._getJSONData();
+
+        expect(res.statusCode).toBe(404);
+        expect(data).toStrictEqual({ error: 'Gasto no encontrado' });
+        expect(next).not.toHaveBeenCalled();
+    });
 });
