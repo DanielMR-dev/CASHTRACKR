face1a8b64b6aa3c581107404a765f1197c803c6
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-07-29 18:23:54 -0500

    258. Testing cuando todo es correcto y un usuario se registra

--- backend/src/tests/integration/app.test.ts
+++ backend/src/tests/integration/app.test.ts
@@ -3,7 +3,6 @@ import server from '../../server';
 import { connectDB } from '../../server';
 import { db } from '../../config/db';
 import { AuthController } from '../../controllers/AuthController';
-import { not } from 'supertest/lib/cookies';
 
 describe('Authentication - Create Account', () => {
     beforeAll(async () => {
@@ -29,13 +28,14 @@ describe('Authentication - Create Account', () => {
     });
 
     test('Should return 400 status code error when the email is invalid', async () => {
+        const userData = {
+            name : "Test name",
+            password : "test_password",
+            email : "not_valid_email"
+        };
         const response = await request(server)
                                     .post('/api/auth/create-account')
-                                    .send({
-                                        name : "Test name",
-                                        password : "test_password",
-                                        email : "not_valid_email"
-                                    });
+                                    .send(userData);
         const mockCreateAccount = jest.spyOn(AuthController, 'createAccount');
         const data = response.body;
 
@@ -48,13 +48,14 @@ describe('Authentication - Create Account', () => {
     });
 
     test('Should return 400 status code error when the password is less than 8 characters', async () => {
+        const userData = {
+            name : "Test name",
+            password : "test",
+            email : "test@test.com"
+        };
         const response = await request(server)
                                     .post('/api/auth/create-account')
-                                    .send({
-                                        name : "Test name",
-                                        password : "test",
-                                        email : "test@test.com"
-                                    });
+                                    .send(userData);
         const mockCreateAccount = jest.spyOn(AuthController, 'createAccount');
         const data = response.body;
 
@@ -64,5 +65,23 @@ describe('Authentication - Create Account', () => {
         expect(data.errors[0].msg).toStrictEqual('La contraseña debe tener al menos 8 caracteres');
         expect(response.statusCode).not.toBe(201);
         expect(mockCreateAccount).not.toHaveBeenCalled();
+    });
+
+    test('Should return 201 status code success when user is created', async () => {
+        const userData = {
+            name : "Test name",
+            password : "test_password",
+            email : "test@test.com"
+        };
+        const response = await request(server)
+                                    .post('/api/auth/create-account')
+                                    .send(userData);
+        const data = response.body;
+
+        expect(response.statusCode).toBe(201);
+        expect(data).toHaveProperty('message');
+        expect(data.message).toBe('¡Tu cuenta ha sido creada correctamente!');
+        expect(response.statusCode).not.toBe(400);
+        expect(data).not.toHaveProperty('errors');
     });
 });
