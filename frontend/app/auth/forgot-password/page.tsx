17ab99509755a768e04626426378970aa78a883c
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-07-31 18:22:51 -0500

    295. Creando la página de Olvidaste tu contraseña

---
+++ frontend/app/auth/forgot-password/page.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import ForgotPasswordForm from "@/components/auth/ForgotPasswordForm";
+
+
+export const metadata: Metadata = {
+    title: "CashTrackr - Olvidé mi Password",
+    description: "Olvidé mi Password",
+};
+
+export default function ForgotPasswordPage() {
+    return (
+        <>
+            <h1 className="font-black text-6xl text-purple-950">¿Olviaste tu Contraseña?</h1>
+            <p className="text-3xl font-bold">aquí puedes <span className="text-amber-500">reestablecerla</span></p>
+            <ForgotPasswordForm />
+        </>
+    );
+}
