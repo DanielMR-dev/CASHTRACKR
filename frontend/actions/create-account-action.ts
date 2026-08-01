d09b9095d36b45a0a22762807daed9bc95bba26b
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-08-01 14:51:23 -0500

    306. Creando un objeto para enviar hacia el backend

--- frontend/actions/create-account-action.ts
+++ frontend/actions/create-account-action.ts
@@ -1,5 +1,11 @@
 "use server"
 
 export async function register(formData: FormData) {
-    console.log(formData);
+    const registerData = {
+        email: formData.get("email"),
+        name: formData.get("name"),
+        password: formData.get("password"),
+        password_confirmation: formData.get("password_confirmation"),
+    }
+    console.log(registerData);
 }
