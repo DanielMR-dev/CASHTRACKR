d51053e5faf48fde3865bb86ddc9671548d4316c
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-08-03 18:00:42 -0500

    validate-token-action implemented

--- frontend/actions/validate-token-action.ts
+++ frontend/actions/validate-token-action.ts
@@ -1,5 +1,7 @@
 "use server"
 
+import { ErrorResponseSchema, SuccessSchema, TokenSchema } from "@/src/schemas";
+
 type ActionStateType = {
     errors: string[];
     success: {
@@ -8,9 +10,34 @@ type ActionStateType = {
 };
 
 export async function validateToken(token: string, prevState: ActionStateType) {
-    console.log("Desde validateToken");
+    const resetPasswordToken = TokenSchema.safeParse(token);
+    if (!resetPasswordToken.success){
+        return {
+            errors: resetPasswordToken.error.issues.map((issue) => issue.message),
+            success: { message: "" },
+        };
+    }
+    const url = `${process.env.API_URL}/auth/validate-token`;
+    const request = await fetch(url, {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            token: resetPasswordToken.data,
+        }),
+    });
+    const data = await request.json();
+    if (request.status !== 200) {
+        const { error } = ErrorResponseSchema.parse(data);
+        return {
+            errors: [error],
+            success: { message: "" },
+        }
+    }
+    const success = SuccessSchema.parse(data);
     return {
         errors: [],
-        success: { message: "" },
+        success
     }
 }
