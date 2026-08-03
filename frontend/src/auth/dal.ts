009cb22630c65a02c79f6b29a9096a59a16fb5cb
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-08-03 14:40:38 -0500

    332. Obtener el JWT desde Cookies

--- frontend/src/auth/dal.ts
+++ frontend/src/auth/dal.ts
@@ -1,5 +1,16 @@
 // Data Access Layer
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation";
 
 export const verifySession = async () => {
-    
+    try {
+        const cookieStore = await cookies();
+        const token = cookieStore.get("CASHTRACKR_TOKEN")?.value;
+        if (!token) {
+            redirect("/auth/login")
+        }
+    } catch (error) {
+        
+    }
+
 }
