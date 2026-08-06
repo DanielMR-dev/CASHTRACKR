27045dc18433cfbf399e64f64f5ba939a98855e0
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-08-05 20:06:49 -0500

    355. Consultar la API para obtener el presupuesto

--- frontend/app/admin/budgets/[id]/edit/page.tsx
+++ frontend/app/admin/budgets/[id]/edit/page.tsx
@@ -1,5 +1,23 @@
+import { getToken } from "@/src/auth/token";
+import { BudgetAPIResponseSchema } from "@/src/schemas";
 
-export default function EditBudgetPage({ params }: {params: {id: string}}) {
+const getBudget = async (budgetId: string) => {
+    const token = await getToken();
+    const url = `${process.env.API_URL}/budgets/${budgetId}`;
+    const request = await fetch(url, {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+    });
+    const data = await request.json();
+    const budget = BudgetAPIResponseSchema.parse(data);
+    return budget;
+}
+
+export default async function EditBudgetPage({ params }: {params: {id: string}}) {
+    const { id } = await params;
+    const budget = await getBudget(id);
     return (
         <div>EditBudgetPage</div>
     );
