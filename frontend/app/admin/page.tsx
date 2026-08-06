c12c11ec6dd706327d7fa5a6b34c3dd808751f5f
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-08-06 09:25:19 -0500

    357. Validando el Presupuesto con un schema de ZOD y página de editar

--- frontend/app/admin/page.tsx
+++ frontend/app/admin/page.tsx
@@ -52,7 +52,7 @@ export default async function AdminPage() {
                             <div className="min-w-0 flex-auto space-y-2">
                                 <p className="text-sm font-semibold leading-6 text-gray-900">
                                     <Link
-                                        href={`/admin/budgets/${budget.id}`}
+                                        href={`/admin/budgets/${budget.id}/edit`}
                                         className="cursor-pointer hover:underline text-2xl font-bold"
                                     >
                                         {budget.name}
