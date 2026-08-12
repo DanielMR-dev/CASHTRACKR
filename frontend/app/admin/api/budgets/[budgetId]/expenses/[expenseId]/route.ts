471050ba6b3b1edec3a67ceee5834236582594e1
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2026-08-12 16:49:19 -0500

    388. Consultando la API para obtener el presupuesto (API Route)

--- frontend/app/admin/api/budgets/[budgetId]/expenses/[expenseId]/route.ts
+++ frontend/app/admin/api/budgets/[budgetId]/expenses/[expenseId]/route.ts
@@ -1,7 +1,19 @@
 import { verifySession } from "@/src/auth/dal";
+import { getToken } from "@/src/auth/token";
 
-export async function GET() {
+export async function GET(req: Request, { params }: { params: { budgetId: string, expenseId: string } }) {
     await verifySession();
-    return Response.json({ msg: 'Obteniendo gasto' });
-
+    const token = await getToken();
+    const url = `${process.env.API_URL}/budgets/${params.budgetId}/expenses/${params.expenseId}`;
+    const request = await fetch(url, {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        }
+    });
+    const data = await request.json();
+    if (!data.ok) {
+        return Response.json(data.error, { status: 403 });
+    }
+    return data;
 }
